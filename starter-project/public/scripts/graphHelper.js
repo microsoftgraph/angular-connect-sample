ea31613fb0b30054fd8ac0db32f9ad3a62b4c946
Jim Crowley <jamescro@microsoft.com>
2017-06-20 15:33:44 -0700

    Updating starter project for MSAL

--- starter-project/public/scripts/graphHelper.js
+++ starter-project/public/scripts/graphHelper.js
@@ -5,16 +5,49 @@
 
 "use strict";
 
+function createApplication(applicationConfig) {
+
+    var clientApplication = new Msal.UserAgentApplication(applicationConfig.clientID, null, function (errorDesc, token, error, tokenType) {
+        // Called after loginRedirect or acquireTokenPopup
+    });
+
+    return clientApplication;
+}
+
+var clientApplication;
+
 (function () {
   angular
     .module('app')
     .service('GraphHelper', ['$http', function ($http) {
 
       // Initialize the auth request
+      clientApplication = createApplication(APPLICATION_CONFIG);
 
       return {
 
         // Sign in and sign out the user
+        login: function login() {
+            clientApplication.loginPopup(APPLICATION_CONFIG.graphScopes).then(function (idToken) {
+                clientApplication.acquireTokenSilent(APPLICATION_CONFIG.graphScopes).then(function (accessToken) {
+                    localStorage.token = accessToken;
+                    window.location.reload();
+                }, function (error) {
+                    clientApplication.acquireTokenPopup(APPLICATION_CONFIG.graphScopes).then(function (accessToken) {
+                        localStorage.token = accessToken;
+                    }, function (error) {
+                        window.alert("Error acquiring the popup:\n" + error);
+                    });
+                })
+            }, function (error) {
+                window.alert("Error during login:\n" + error);
+            });
+        },
+        logout: function logout() {
+            clientApplication.logout();
+            delete localStorage.token;
+            delete localStorage.user;
+        },
 
         // Get the profile of the current user
 
